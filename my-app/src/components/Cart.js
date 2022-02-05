f45e665302e9fcf42f90713379653301d943dbd7
Gianina Sol Rossi <gianinarossi@Gianinas-MacBook-Pro.local>
2022-02-05 20:43:49 -0300

    finished

--- my-app/src/components/Cart.js
+++ my-app/src/components/Cart.js
@@ -61,55 +61,64 @@ const Cart = (props) => {
   }
 
   return (
-    <div className="cartView">
-      {cart.map((prod) => (
-        <ProductItem key={prod.id} data={prod} />
-      ))}
+    <>
+      <div className="cartView">
+        {cart.map((prod) => (
+          <ProductItem key={prod.id} data={prod} />
+        ))}
 
-      <Button
-        className="m-2"
-        variant="primary"
-        onClick={() => {
-          dispatch({
-            type: "CLEAR_CART",
-          });
-        }}
-      >
-        Vaciar Carrito
-      </Button>
-      <Link to="/">
-        <Button style={{ margin: "0 10" }} variant="success">
-          Back to Home
-        </Button>
-      </Link>
-
-      <Form style={{position:'absolute' , left:'50%' , transform:'translateX(-50%)'}}>
-        <Form.Group className="mb-3" >
-          <Form.Label>Nombre</Form.Label>
-          <Form.Control id="name" type="name" placeholder="Name" />
-        </Form.Group>
-        <Form.Group className="mb-3" >
-          <Form.Label>Email</Form.Label>
-          <Form.Control id="mail" type="text" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            Relax! No vamos a compartir tus datos, es solo para registrar la compra
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" >
-          <Form.Label>Cel</Form.Label>
-          <Form.Control id="phone" type="phone" placeholder="Phone" />
-        </Form.Group>
-        TOTAL: $ {totalPrice}
         <Button
+          className="m-2"
           variant="primary"
-          type="submit"
-          style={{ marginLeft: 8 }}
-          onClick={SendOrder}
+          onClick={() => {
+            dispatch({
+              type: "CLEAR_CART",
+            });
+          }}
         >
-          Comprar
+          Vaciar Carrito
         </Button>
-      </Form>
-    </div>
+        <Link to="/">
+          <Button style={{ margin: "0 10" }} variant="success">
+            Back to Home
+          </Button>
+        </Link>
+
+        <Form
+          style={{
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          <Form.Group className="mb-3">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control id="name" type="name" placeholder="Name" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control id="mail" type="text" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              Relax! No vamos a compartir tus datos, es solo para registrar la
+              compra
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Cel</Form.Label>
+            <Form.Control id="phone" type="phone" placeholder="Phone" />
+          </Form.Group>
+          TOTAL: $ {totalPrice}
+        </Form>
+      </div>
+      <Button
+        variant="primary"
+        type="submit"
+        style={{ marginLeft: 8 }}
+        onClick={SendOrder}
+      >
+        Comprar
+      </Button>
+    </>
   );
 };
 
